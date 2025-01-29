65465c60fdc039e3eafe30c8bedf4d5889916c5d
Harsharaj Shisodiya <harsharaj.shisodiya@cronixweb.com>
2025-01-29 21:12:38 +0530

    Removed additional slider images

--- core/components/slideshow/index.tsx
+++ core/components/slideshow/index.tsx
@@ -6,32 +6,32 @@ const slides = [
   {
     cta: { label: 'Shop now', href: '/#' },
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+      'This demo site shows off the BigCommerce Catalyst frontend connected to a HawkSearch index.',
     image: {
       src: SlideshowBG,
-      altText: 'An assortment of brandless products against a blank background',
+      altText: 'An image of an employee working with multimeter',
       blurDataUrl:
         'data:image/jpeg;base64,/9j/4QC8RXhpZgAASUkqAAgAAAAGABIBAwABAAAAAQAAABoBBQABAAAAVgAAABsBBQABAAAAXgAAACgBAwABAAAAAgAAABMCAwABAAAAAQAAAGmHBAABAAAAZgAAAAAAAABIAAAAAQAAAEgAAAABAAAABgAAkAcABAAAADAyMTABkQcABAAAAAECAwAAoAcABAAAADAxMDABoAMAAQAAAP//AAACoAQAAQAAAAoAAAADoAQAAQAAAAcAAAAAAAAA/9sAQwADAgIDAgIDAwMDBAMDBAUIBQUEBAUKBwcGCAwKDAwLCgsLDQ4SEA0OEQ4LCxAWEBETFBUVFQwPFxgWFBgSFBUU/9sAQwEDBAQFBAUJBQUJFA0LDRQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQU/8AAEQgABwAKAwERAAIRAQMRAf/EABUAAQEAAAAAAAAAAAAAAAAAAAMJ/8QAIBAAAQQBBAMAAAAAAAAAAAAAAQIDBAURABIhMQYjgf/EABYBAQEBAAAAAAAAAAAAAAAAAAEAAv/EABkRAAIDAQAAAAAAAAAAAAAAAAARAQIhQf/aAAwDAQACEQMRAD8AoZ5EzayKWW3Syo0GyKPTJlsF9ts9klsKTu46GQOfms2awJfAKywmt1sRNgqK7PS0gSHI4WltTmBuKQckJJzgE9aYa0tP/9k=',
     },
     key: 1,
-    title: '25% Off Sale',
+    title: 'HawkSearch Demo',
   },
 
-  {
-    cta: { label: 'Shop now', href: '/#' },
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-    key: 2,
-    title: 'Great Deals',
-  },
+  // {
+  //   cta: { label: 'Shop now', href: '/#' },
+  //   description:
+  //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+  //   key: 2,
+  //   title: 'Great Deals',
+  // },
 
-  {
-    cta: { label: 'Shop now', href: '/#' },
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-    key: 3,
-    title: 'Low Prices',
-  },
+  // {
+  //   cta: { label: 'Shop now', href: '/#' },
+  //   description:
+  //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+  //   key: 3,
+  //   title: 'Low Prices',
+  // },
 ];
 
 export const Slideshow = () => <ComponentsSlideshow slides={slides} />;
